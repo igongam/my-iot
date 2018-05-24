e5f8f32c0665506d7446ce84d183b546df33f11d
igongam <igongam@naver.com>
2018-05-24 15:09:09 +0900

    added

--- mysite/urls.py
+++ mysite/urls.py
@@ -23,6 +23,6 @@ from iottest.views import humidity
 urlpatterns = [
     url(r'^keyboard/', keyboard, name='keyboard'),
     url(r'^message',answer,name='answer'),
-    url(r'^temperature/(?P<temp>\d+)$',temperature,name='temperature'),
-    url(r'^humidity/(?P<humi>\d+)$',humidity,name='humidity'),
+    url(r'^temperature/(?P<temp>\w+)$',temperature,name='temperature'),
+    url(r'^humidity/(?P<humi>\w+)$',humidity,name='humidity'),
 ]
